230fd3ed768f05488c7d64935aa03ec9db6e14d1
Gustavo Garcia <gustavogarcia42@yahoo.com>
2026-03-31 20:03:39 -0500

    going back to allowing just frontend url

--- server.ts
+++ server.ts
@@ -8,9 +8,9 @@ const app = new Elysia()
   .use(
     cors({
       origin: Bun.env.VITE_FRONTEND_URL,
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-      preflight: true,
+      // credentials: true,
+      // allowedHeaders: ["Content-Type", "Authorization"],
+      // preflight: true,
     }),
   )
   // Define a route to handle email data sent from the frontend
