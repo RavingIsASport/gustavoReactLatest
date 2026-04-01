986a47ec1a0d717e122f4b341ccfc260d40ef363
Gustavo Garcia <gustavogarcia42@yahoo.com>
2026-03-31 19:47:00 -0500

    needed my routes to chain in elysia server

--- server.ts
+++ server.ts
@@ -2,43 +2,39 @@ import { Elysia, t } from "elysia";
 import { Resend } from "resend";
 import { cors } from "@elysiajs/cors";
 
-const app = new Elysia();
-app.use(
-  cors({
-    origin: Bun.env.VITE_FRONTEND_URL,
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
 const resend = new Resend(Bun.env.VITE_RESEND_API_KEY);
 
-// Define a route to handle email data sent from the frontend
-app.post("/api/email", async (ctx) => {
-  // Getting data from the request body
-  const data: any = await ctx.body;
+const app = new Elysia()
+  .use(
+    cors({
+      origin: Bun.env.VITE_FRONTEND_URL,
+      credentials: true,
+    }),
+  )
+  // Define a route to handle email data sent from the frontend
+  .post("/api/email", async (ctx) => {
+    // Getting data from the request body
+    const data: any = await ctx.body;
 
-  // Console log the received email data for debugging purposes
-  console.log("Received email data:", data);
+    // Console log the received email data for debugging purposes
+    console.log("Received email data:", data);
 
-  // await resend.emails.send({
-  //   from: "onboarding@resend.dev",
-  //   to: Bun.env.VITE_EMAIL!,
-  //   subject: data.subject || "New Message from Contact From" + data.name,
-  //   html: `
-  //         <h2><strong>Name:</strong> ${data.name}</h2>
-  //         <p><strong>Reply-to Email:</strong> ${data.email}</p>
-  //         <p><strong>Message:</strong></p>
-  //         <p>${data.message}</p>
-  //         `,
-  //   replyTo: data.email,
-  // });
+    // await resend.emails.send({
+    //   from: "onboarding@resend.dev",
+    //   to: Bun.env.VITE_EMAIL!,
+    //   subject: data.subject || "New Message from Contact From" + data.name,
+    //   html: `
+    //         <h2><strong>Name:</strong> ${data.name}</h2>
+    //         <p><strong>Reply-to Email:</strong> ${data.email}</p>
+    //         <p><strong>Message:</strong></p>
+    //         <p>${data.message}</p>
+    //         `,
+    //   replyTo: data.email,
+    // });
 
-  // Here you can add logic to send the email using a service like nodemailer or an email API
-  return { message: "Email data received successfully at backend", data };
-});
-
-app.listen(Bun.env.PORT || 3000, () => {
-  console.log(
-    "Server is running on http://localhost:" + (Bun.env.PORT || 3000),
-  );
-});
+    // Here you can add logic to send the email using a service like nodemailer or an email API
+    return { message: "Email data received successfully at backend", data };
+  })
+  .listen(Bun.env.PORT || 3000, () => {
+    console.log("Server is running!");
+  });
